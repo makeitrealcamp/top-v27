fcd9cf26f46c64a860b00a0991d2a866b3d843bd
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-03-02 07:56:20 -0500

    routing

--- examples/react/routing/src/App.js
+++ examples/react/routing/src/App.js
@@ -12,7 +12,7 @@ function App() {
       <Header/> 
       <Routes>
         <Route path='/' element={<HomePage/>}/>
-        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/about-us' element={<AboutPage/>}/>
         <Route path='*' element={<NotFoundPage/>}/>
       </Routes>
     </div>
