603fa1e40e5776a7ff8d0af80fcdf13e7fa364c4
Sergio Jaramillo Luna <sergiojaramillo@mac-sergio.local>
2023-03-02 15:38:32 -0500

    clase de hooks primera parte

---
+++ examples/react/hooks/src/App.js
@@ -0,0 +1,48 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {useState} from 'react'
+//useState, useEffect
+//useMemo, useRef, customHooks
+
+// Hooks es una función que nos permite enganchar el estado y los ciclos a los componentes 
+
+const arr = [ 1, 2, 3, 4 ]
+
+function App() {
+  const [ name, setName ] = useState('Jhon Doe')
+  const [ array, setArray ] = useState([])
+  const [ bool, setBool ] = useState(false)
+  const [count, setCount] = useState(0)
+  const [ state, setState ] = useState({ //Tener cuidado de no mutar el estado objeto
+      name: 'Jhon',
+      age: 33
+    })
+
+
+  //1. Los hooks solo se usan dentro de otros hooks y de componentes función en react
+  //2. Los hooks siempre se ejecutan en el mismo orden
+  //3. Los hooks siempre se deben ejecutar en el primer nivel del componente
+  //4. Los hooks siempre se nombrar con el prefijo use
+
+  const handleClick = () => {
+    setCount((state) => state + 1)
+    //setState({ ...state, name: 'Camilo' })
+    //setCount(count + 1)
+  }
+
+  console.log("state", state)
+
+  return (
+    <div className="App-header">
+        { bool && <img src={logo} className="App-logo" alt="logo" />}
+
+        <button onClick={() => setBool(state => !state)}>Ver o no</button>
+        
+        <h1>Clases de hooks, estados y ciclos de vida</h1>
+        <button onClick={handleClick}>Sumar</button>
+        <h2>{count}</h2>
+    </div>
+  );
+}
+
+export default App;
